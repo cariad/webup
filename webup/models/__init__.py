bcc74cb19d0c61e83054350111a39dfa5857123b
Cariad Eccleston <cariad@cariad.earth>
2021-12-24 17:35:31 +0000

    Remove DirectoryUploadResult

--- webup/models/__init__.py
+++ webup/models/__init__.py
@@ -1,9 +1,7 @@
-from webup.models.directory_upload_result import DirectoryUploadResult
 from webup.models.file import File
 from webup.models.upload_result import UploadResult
 
 __all__ = [
-    "DirectoryUploadResult",
     "File",
     "UploadResult",
 ]
--- webup/models/directory_upload_result.py
+++
@@ -1,6 +0,0 @@
-from dataclasses import dataclass
-
-
-@dataclass
-class DirectoryUploadResult:
-    process_count: int
