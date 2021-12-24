76bd1043c4c23cc1a67ad77ce22c1736b4acae64
Cariad Eccleston <cariad@cariad.earth>
2021-12-24 15:45:56 +0000

    Add models

---
+++ webup/models/__init__.py
@@ -0,0 +1,9 @@
+from webup.models.directory_upload_result import DirectoryUploadResult
+from webup.models.file import File
+from webup.models.upload_result import UploadResult
+
+__all__ = [
+    "DirectoryUploadResult",
+    "File",
+    "UploadResult",
+]
